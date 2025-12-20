af685a1db7324f743707345bdd5a6e4fd5ab8dc7
MANISH6148 <manishbobburi6148@gmail.com>
2025-12-21 00:10:30 +0530

    initial project configuration

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="text-4xl font-mono font-bold">
+        <h1>Inside <span className="text-blue-500">{"<"}Div{"/>"}</span> tag</h1>
+        <h3 className="text-2xl font-medium">Manish Bobburi</h3>
+      </div>
+    </>
+  )
+}
+
+export default App
