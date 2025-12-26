f016e9c27f83bf4aa442d3b2395da67f2792b39d
MANISH6148 <manishbobburi6148@gmail.com>
2025-12-27 00:00:29 +0530

    feat(componenets): introduce resuable Card component

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,27 @@
+import type { ReactNode } from 'react';
+
+type CardProps = {
+    children: ReactNode;
+    className?: string;
+    onClick?: () => void;
+}
+
+export default function Card({children, className = "", onClick}: CardProps) {
+    return (
+        <div
+            onClick={onClick}
+            className={`
+                bg-white 
+                border border-gray-200 
+                rounded-xl 
+                p-4 
+                shadow-sm 
+                hover:shadow-md 
+                transition
+                ${className}
+            `}
+        >
+        {children}
+        </div>
+    );
+}
