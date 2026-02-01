55f1d6b1edcb69c6c916cf75335f3fabe2a84cb0
MANISH6148 <manishbobburi6148@gmail.com>
2026-02-01 20:39:48 +0530

    feat(posts): add edit/delete dropdown for posts

--- src/features/posts/components/PostCard.tsx
+++ src/features/posts/components/PostCard.tsx
@@ -1,37 +1,58 @@
-import Card from '../../../components/Card';
-import DifficultyBadge from '../../../components/DifficultyBadge';
-import type { PostProps } from '../post.types';
+import { useState } from "react";
+import { EllipsisVertical } from "lucide-react";
+import Card from "../../../components/Card";
+import type { PostProps } from "../post.types";
+import Button from "../../../components/Button";
+import { timeAgo } from "../../../utils/timeAgo";
+import { useAuth } from "../../auth/auth.context";
+import DropDown from "../../../components/DropDown";
+import { deletePost } from '../../../services/posts.api';
+import DifficultyBadge from "../../../components/DifficultyBadge";
 
 type PostCardProps = {
-  post: PostProps
-}
+  post: PostProps;
+};
 
 export default function PostCard({ post }: PostCardProps) {
-  return (
-    <Card>
+  const [isClicked, setIsClicked] = useState(false);
+  const createdAt = timeAgo(post.createdAt);
+  const { user } = useAuth();
 
+  return (
+    <Card className="my-4">
       <div className="flex justify-between items-start">
         <div>
           <h2 className="font-semibold text-lg">{post.company}</h2>
           <p className="text-sm text-gray-500">{post.role}</p>
         </div>
-        <DifficultyBadge level={post.difficulty} />
+
+        <DifficultyBadge level={post.overallDifficulty} />
+
+        {user?.id === post.userId && (
+          <div className="relative">
+            <Button
+              variant="ghost"
+              size="sm"
+              className="cursor-pointer"
+              onClick={() => setIsClicked((prev) => !prev)}
+            >
+              <EllipsisVertical size={15}/>
+            </Button>
+
+            {isClicked && <DropDown onClose={() => setIsClicked(false)} onDelete={async () => {
+             await  deletePost(post.id)
+            }} onEdit={() => console.log("Clicked Edit")}/>}
+          </div>
+        )}
       </div>
 
-
       <p className="text-sm text-gray-700 mt-3 line-clamp-3">
-        {post.summary || "Grind Leetcode till your fingers **"}
+        {post.body || "N/A"}
       </p>
 
-
-      <div className="flex justify-between items-center text-sm text-gray-500 mt-4">
-        <div className="flex gap-4">
-          <span>⬆ {post.upvotes}</span>
-          <span>⬇ {post.downvotes}</span>
-          <span>💬 {post.commentsCount}</span>
-        </div>
+      <div className="flex justify-end items-center text-sm text-gray-500 mt-4">
         <span>
-          {post.isAnonymous ? "Anonymous" : "User"} • {post.createdAt}
+          {post.displayName} • {createdAt}
         </span>
       </div>
     </Card>
