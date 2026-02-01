959478d3649355dc89fd8326fb8dbeada83daf71
MANISH6148 <manishbobburi6148@gmail.com>
2026-02-01 20:51:35 +0530

    refactor(posts): format PostsPage

--- src/features/posts/pages/PostsPage.tsx
+++ src/features/posts/pages/PostsPage.tsx
@@ -1,21 +1,20 @@
-import { useLoaderData  } from 'react-router-dom';
-import PostCard from '../components/PostCard';
-import type { PostProps } from '../post.types';
-import type { ApiResponse } from '../../../types';
+import { useLoaderData } from "react-router-dom";
+import PostCard from "../components/PostCard";
+import type { PostProps } from "../post.types";
+import type { ApiResponse } from "../../../types";
 
 function PostsPage() {
   const posts = useLoaderData<ApiResponse<PostProps[]>>();
-  
-  if(posts.data.length === 0) return <div>No posts found.</div>
+
+  if (posts.data.length === 0) return <div>No posts found.</div>;
 
   return (
-    <div className='pt-15'>
-      {posts.data.map(post => (
-          <PostCard key={post.id} post={post}/>
-        )
-      )}
+    <div className="pt-15">
+      {posts.data.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
     </div>
-  )
+  );
 }
 
 export default PostsPage;
