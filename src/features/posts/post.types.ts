629273f37d7cc925aea1409a6065d8d201492ad0
MANISH6148 <manishbobburi6148@gmail.com>
2026-02-13 00:59:29 +0530

    feat(posts): add type Cursor and PaginatedPosts

--- src/features/posts/post.types.ts
+++ src/features/posts/post.types.ts
@@ -5,23 +5,43 @@ type Difficulty =
   | 'Medium_Hard'
   | 'Hard';
 
+
+export type Cursor = {
+  createdAt: string;
+  id: number;
+}
+
 export type PostProps = {
   id: number;
+  userId: number;
   company: string;
   role: string;
-  difficulty: Difficulty;
-  summary: string;
+  overallDifficulty: Difficulty;
+  body: string;
   upvotes: number;
   downvotes: number;
   commentsCount: number;
   isAnonymous: boolean;
   createdAt: string;
+  displayName: string;
+  user?: userObj;
 };
 
+export type PaginatedPosts = {
+  items: PostProps[];
+  nextCursor: Cursor | null;
+  hasMore: boolean;
+}
+
 export interface CreatePostPayload {
+  userId: number | undefined;
   company: string;
   role: string;
   difficulty: Difficulty;
   summary: string;
   isAnonymous: boolean;
+}
+
+type userObj = {
+  name: string;
 }
