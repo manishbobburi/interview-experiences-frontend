be615a6f2627fb404b151c56adbcc6e5b7cb355e
MANISH6148 <manishbobburi6148@gmail.com>
2026-02-13 17:45:11 +0530

    fix(utils): add missing 's' suffix in seconds time formatter

--- src/utils/timeAgo.ts
+++ src/utils/timeAgo.ts
@@ -2,7 +2,7 @@ export function timeAgo(timestampMs: string) {
     const diff  = Date.now() - new Date(timestampMs).getTime();;
 
     const seconds = Math.floor(diff/1000);
-    if(seconds < 60) return `${seconds} ago`;
+    if(seconds < 60) return `${seconds}s ago`;
 
     const minutes = Math.floor(seconds / 60);
     if (minutes < 60) return `${minutes}m ago`;
