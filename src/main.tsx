2cda59435cfd272620e8aa596d767e4032c68849
MANISH6148 <manishbobburi6148@gmail.com>
2026-02-17 15:33:53 +0530

    wrap app with toast context provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/router';
 import { AuthProvider } from './features/auth/auth.context';
+import { ToastProvider } from './context/ToastContext';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router}/>
-   </AuthProvider>
+    <ToastProvider>
+      <AuthProvider>
+        <RouterProvider router={router}/>
+    </AuthProvider>
+   </ToastProvider>
   </StrictMode>,
 )
