106c6e1191d8702d92b53c0d451eb94dd737b6f7
MANISH6148 <manishbobburi6148@gmail.com>
2026-03-04 15:19:08 +0530

    feat(posts): add CompanySearch scrollable list for selecting company

---
+++ src/features/posts/components/CompanySearch.tsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+import type { Company } from "../post.types";
+import { getCompanies } from "../../../services/company.api";
+
+interface CompanySearchProps {
+  handleSelect: (company: Company) => void;
+}
+
+export default function CompanySearch({ handleSelect }: CompanySearchProps) {
+  const [query, setQuery] = useState("");
+  const [companies, setCompanies] = useState<Company[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [isFocused, setIsFocused] = useState(false);
+
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      setLoading(true);
+      const response = await getCompanies();
+
+      if (response.success) {
+        setCompanies(response.data);
+      } else {
+        setError("Failed to load company details");
+      }
+      setLoading(false);
+    };
+
+    fetchCompanies();
+  }, []);
+
+  const filteredCompanies =
+    query.length === 0
+      ? companies
+      : companies.filter((company) =>
+          company.name.toLowerCase().startsWith(query.toLowerCase())
+        );
+
+  return (
+    <div className="relative w-full max-w-md">
+      <input
+        type="text"
+        placeholder="Company"
+        value={query}
+        onFocus={() => setIsFocused(true)}
+        onChange={(e) => setQuery(e.target.value)}
+        className="w-full border p-2"
+      />
+
+      {loading && <p>Loading...</p>}
+      {error && <p className="text-red-500 text-sm">{error}</p>}
+
+      {isFocused && filteredCompanies.length > 0 && (
+        <ul className="absolute w-full bg-white shadow-md rounded mt-1 z-10 max-h-60 overflow-y-auto">
+          {filteredCompanies.map((company) => (
+            <li
+              key={company.id}
+              onClick={() => {
+                handleSelect(company);
+                setQuery(company.name);
+                setIsFocused(false);
+              }}
+              className="flex items-center gap-x-1 p-2 hover:bg-gray-100 cursor-pointer border border-gray-300 rounded m-0.5"
+            >
+              <div className="w-5 h-5">
+                <img src={company.logoUrl} alt="" className="w-full h-full object-contain"/>
+              </div>
+              {company.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
