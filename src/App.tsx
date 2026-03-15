4905a34035e8074b1a07938bc2126666f7a3f7be
MANISH6148 <manishbobburi6148@gmail.com>
2026-03-15 22:49:22 +0530

    feat(ui): show create post only in home and profile routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation  } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import CreatePostButton from './features/posts/components/CreatePostButton';
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
+  const location = useLocation();
+
+  const showCreatePostButton = location.pathname === "/" || location.pathname.startsWith("/user/");
 
   return (
     <>
     <Navbar setSearchQuery={setSearchQuery} searchQuery={searchQuery}/>
     <Outlet context={{ searchQuery }}/>
-    <CreatePostButton />
+    { showCreatePostButton && <CreatePostButton />}
     </>
   )
 }
